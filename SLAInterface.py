a8a1ce5dcf9364849f5983c93b18d643ade45fb3
NotZack <zackarynichol@gmail.com>
2020-01-04 19:26:33 -0600

    Added specific room querying and primitive room data display

--- SLAInterface.py
+++ SLAInterface.py
@@ -1,11 +1,13 @@
 import socket
 import kivy
+import re
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.config import Config
+from kivy.uix.label import Label
 
 Config.set('graphics', 'width', '1920')
 Config.set('graphics', 'height', '1020')
@@ -20,23 +22,27 @@ def open_socket(port_number):
     client_socket.connect((host, port))
 
 
-def send_to_socket(message):
+def send_query_to_socket(message):
     client_socket.sendall((message + '\n').encode())
-    data = client_socket.recv(1024).decode()
-    print(data)
+
+    data = client_socket.recv(2048).decode()
     return data
 
 
-class SearchBar(FloatLayout):
+def collect_room_data(room_string):
+    return send_query_to_socket("Collect_Unique_Room_Data:" + room_string)
+
+
+class SearchComponents(FloatLayout):
 
     def __init__(self, **kwargs):
-        super(SearchBar, self).__init__(**kwargs)
+        super(SearchComponents, self).__init__(**kwargs)
 
         self.search_input = TextInput(multiline=False, size_hint=(0.10, 0.033), pos_hint={'x': 0.05, 'y': 0.90})
         self.search_input.bind(text=self.on_type)
 
         self.search_button = Button(text='Search', size_hint=(0.10, 0.033), pos_hint={'x': 0.15, 'y': 0.90})
-        self.search_button.bind(on_press=self.send_exact_search)
+        self.search_button.bind(on_press=self.on_search_press)
 
         self.search_results = GridLayout(cols=1, size_hint=(0.2, 0.25), pos_hint={'x': 0.05, 'y': 0.65})
 
@@ -44,28 +50,38 @@ class SearchBar(FloatLayout):
         self.add_widget(self.search_input)
         self.add_widget(self.search_results)
 
-    def send_exact_search(self, button):
-        print("Search button pressed")
-        send_to_socket(self.search_input.text)
+    def on_search_press(self, button):
+        send_query_to_socket(self.search_input.text)
 
     def on_type(self, instance, text):
         self.search_results.clear_widgets()
 
-        for data in send_to_socket(self.search_input.text).split(','):
-            if len(data) > 2:
-                # , background_normal='', background_color=(0,0,0,0)
-                result_button = Button(text=data, size_hint=(0.1, 0.03))
-                result_button.bind(on_press=self.update_textfield_text)
-                self.search_results.add_widget(result_button)
+        query_result = send_query_to_socket(self.search_input.text)
+        if query_result is not None:
+            for data in query_result.split(','):
+                if len(data) > 2:
+                    # , background_normal='', background_color=(0,0,0,0)
+                    result_button = Button(text=data, size_hint=(0.1, 0.03))
+                    result_button.bind(on_press=self.show_room_data)
+                    self.search_results.add_widget(result_button)
 
-    def update_textfield_text(self, button):
+    def show_room_data(self, button):
         self.search_input.text = button.text
-        self.send_exact_search(None)
+        room_data = collect_room_data(button.text)
+        room_data = room_data[1:-1]
+
+        pos_offset = 20
+        for session in room_data.split(']'):
+            test_label = Label(text=session, pos=(400, 600 - pos_offset), size_hint=(0.5, 0.03))
+            self.add_widget(test_label)
+            pos_offset += 20
+
+        print(room_data)
 
 
 class InterfaceGUIApplication(App):
     def build(self):
-        return SearchBar()
+        return SearchComponents()
 
 
 if __name__ == '__main__':
